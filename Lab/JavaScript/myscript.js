cc55ada93ec1ae681d9f1b3ce25bf007a7ffc522
Sandra Eriksen <59083841+sandra1995linnea@users.noreply.github.com>
2021-09-06 22:56:42 +0200

    update of pic position

--- Lab/JavaScript/myscript.js
+++ Lab/JavaScript/myscript.js
@@ -1,6 +1,24 @@
 
+
 // this method should change the three pieces of news on row 1
 displayNews()
 {
-    
+    window.alert("Hello world!");
+/*
+    var news = document.getElementsByClassName("newsimage");
+    var newsIndex = 0;
+
+    for(i = 0; i < news.length; i++)
+    {
+        news[i].style.display = "none";
+    }
+    newsIndex++;
+
+    if(newsIndex < news.length)
+    {
+        news[i - 1].style.display = "none";
+        setTimeout()
+    }
+*/
+
 }
