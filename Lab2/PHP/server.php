9616166653aaf892ee1746f5cf34a7418ce30cb6
Sandra Eriksen <59083841+sandra1995linnea@users.noreply.github.com>
2021-09-28 15:10:46 +0200

    .

--- Lab2/PHP/server.php
+++ Lab2/PHP/server.php
@@ -1,4 +1,5 @@
 <?php
 // this PHP script is to be used for displaying news in the grid boxes
     $news = json_decode(file_get_contents('../JSON/news.json'), true);
+    echo json_encode($news);
 ?>
