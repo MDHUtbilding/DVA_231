25fb0d3d32147e4ef81122af1ba1b44a0e191d54
Sandra Eriksen <59083841+sandra1995linnea@users.noreply.github.com>
2021-09-28 19:54:04 +0200

    script

--- Lab2/JavaScript/script.js
+++ Lab2/JavaScript/script.js
@@ -1,5 +1,30 @@
 
 function createTopNews()
 {
-    
+    div=$("<div />", 
+    {
+        "class": "image1"
+    });
 }
+
+function addNews()
+{
+
+}
+
+function carousel()
+	{
+            var i;
+	    var x = document.getElementsByClassName("carouselImg");
+			
+	    for (i = 0; i < x.length; i++)
+	        x[i].style.display = "none";  
+
+            myIndex++;
+
+            if (myIndex > x.length)
+	        myIndex = 1
+
+            x[myIndex-1].style.display = "block";  
+	    setTimeout(carousel, 30000); // Change image every 30 seconds
+        }
