3935dbed3ac142f585ec6e082679a861a4797dc5
MDHUtbilding <89913551+MDHUtbilding@users.noreply.github.com>
2021-09-28 14:40:48 +0200

    Add files via upload
    How to read JSON file.

---
+++ Lab2/readJSON.php
@@ -0,0 +1,17 @@
+<?php
+
+    $people_json = file_get_contents('file.json');
+    $decoded_json = json_decode($people_json, true);
+    $customers = $decoded_json['customers'];
+
+    foreach($customers as $customer)
+    {
+        $name = $customer['name'];
+        $countries = $customer['countries'];
+
+        foreach($countries as $country) 
+        {
+            echo $name.' visited '.$country['name'].' in '.$country['year'].'.';
+        }
+    }
+?>
